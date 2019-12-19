3c90c366b2ecad16ff8922228f9206a84729ad7e
saikrishnajami <saikrishnajami60@gmail.com>
2019-12-19 14:40:34 +0530

    This file tells about non primitive data type string

---
+++ string.java
@@ -0,0 +1,17 @@
+public class string {
+	public static void main (String args[]) {
+		int MySai = 7;
+	    String text = "Hello";
+	    String sai = "Welcome";
+	    String jami = " ";
+	    String krish = text + jami + sai;
+	    
+	    System.out.println(MySai);
+	    System.out.println(text);
+	    System.out.println(krish);
+	    System.out.println("Hello" + " " + "Welcome");
+	    System.out.println("My Number is " +  MySai);
+	    double My= 7.8;
+	    System.out.println("My number is:" +My + ".");
+	}
+}
